bcaebaa93625479c397fe938dc59c47d3a8566b2
abhishek62682 <abhishekreact.dev@gmail.com>
2026-07-17 10:17:14 +0530

    v2

--- src/components/home/ServicesSection.jsx
+++ src/components/home/ServicesSection.jsx
@@ -71,7 +71,7 @@ export default function ServicesSection() {
       </div>
 
       {/* ===== Section Content ===== */}
-      <div className="section-content  overflow-hidden grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
+      <div className="section-content  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
         {services.map(({ img, title, desc }) => (
           <div
             key={title}
